bdeaabaae837b95330f557f161e318a8e11f4391
Václav Pavlíček <vendasky@email.cz>
2021-12-01 13:13:02 +0000

    fix bit shifts in assembler unit tests

--- utils/test/assembler.cc
+++ utils/test/assembler.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <bitset>
 #include <regex>
 #include <vector>
 #include <map>
@@ -24,9 +25,11 @@ public:
 
 class Opcodes {
 public:
-    const static int JR = 0b000000;
     const static int ADDU = 0b000000;
     const static int ADDIU = 0b001001;
+    const static int AND = 0b000000;
+    const static int ANDI = 0b001100;
+    const static int JR = 0b000000;
     const static int LW = 0b100011;
     const static int SW = 0b101011;
 };
@@ -98,7 +101,7 @@ int register_name_to_index(const std::string &registerName) {
     }
 }
 
-int convert_immediate_const_to_int(std::string immediateConst) {
+unsigned int convert_immediate_const_to_int(std::string immediateConst) {
     immediateConst = trim(immediateConst);
 
     if (immediateConst.substr(0, 2) == "0x") {
@@ -113,28 +116,38 @@ int convert_immediate_const_to_int(std::string immediateConst) {
 std::map<std::string, InstructionParseConfig> initializeConfigMap() {
     std::map<std::string, InstructionParseConfig> configs;
 
+    std::regex andRegex("AND (.+), (.+), (.+)");
+    std::vector<int> andShifts{11, 21, 16};
+    InstructionParseConfig AND_CONFIG(andRegex, Opcodes::AND, 0b100100, andShifts);
+    configs.insert(std::pair<std::string, InstructionParseConfig>("AND", AND_CONFIG));
+
+    std::regex andiRegex("ANDI (.+), (.+), (.+)");
+    std::vector<int> andiShifts{16, 21, -1};
+    InstructionParseConfig ANDI_CONFIG(andiRegex, Opcodes::ANDI, 0, andiShifts);
+    configs.insert(std::pair<std::string, InstructionParseConfig>("ANDI", ANDI_CONFIG));
+
     std::regex adduRegex("ADDU (.+), (.+), (.+)");
-    std::vector<int> adduShifts{10, 20, 15};
+    std::vector<int> adduShifts{11, 21, 16};
     InstructionParseConfig ADDU_CONFIG(adduRegex, Opcodes::ADDU, 0b100001, adduShifts);
     configs.insert(std::pair<std::string, InstructionParseConfig>("ADDU", ADDU_CONFIG));
 
     std::regex adduiRegex("ADDIU (.+), (.+), (.+)");
-    std::vector<int> adduiShifts{15, 20, -1};
+    std::vector<int> adduiShifts{16, 21, -1};
     InstructionParseConfig ADDIU_CONFIG(adduiRegex, Opcodes::ADDIU, 0, adduiShifts);
     configs.insert(std::pair<std::string, InstructionParseConfig>("ADDIU", ADDIU_CONFIG));
 
     std::regex jrRegex("JR (.+)");
-    std::vector<int> jrShifts{20};
+    std::vector<int> jrShifts{21};
     InstructionParseConfig JR_CONFIG(jrRegex, Opcodes::JR, 0b1000, jrShifts);
     configs.insert(std::pair<std::string, InstructionParseConfig>("JR", JR_CONFIG));
 
     std::regex lwRegex("LW (.+), (.+)\\((.+)\\)");
-    std::vector<int> lwShifts{15, -1, 20};
+    std::vector<int> lwShifts{16, -1, 21};
     InstructionParseConfig LW_CONFIG(lwRegex, Opcodes::LW, 0, lwShifts);
     configs.insert(std::pair<std::string, InstructionParseConfig>("LW", LW_CONFIG));
 
     std::regex swRegex("SW (.+), (.+)\\((.+)\\)");
-    std::vector<int> swShifts{15, -1, 20};
+    std::vector<int> swShifts{16, -1, 21};
     InstructionParseConfig SW_CONFIG(swRegex, Opcodes::SW, 0, swShifts);
     configs.insert(std::pair<std::string, InstructionParseConfig>("SW", SW_CONFIG));
 
@@ -143,27 +156,34 @@ std::map<std::string, InstructionParseConfig> initializeConfigMap() {
 
 std::string convert_instruction_to_hex(const std::string &command,
                                        const std::map<std::string, InstructionParseConfig> configs) {
-    int code = 0;
+    unsigned int code = 0;
     std::string trimmedCommand = trim(command);
     std::string instrName = trimmedCommand.substr(0, trimmedCommand.find(" "));
-    InstructionParseConfig config = configs.find(instrName)->second;
-    code += (config.getOpcode() << 25);
+    auto it = configs.find(instrName);
 
-    std::smatch matches;
-    if (std::regex_search(command, matches, config.getRegex())) {
-        for (int i = 1; i < matches.size(); i++) {
-            int bitShift = config.getBitShifts()[i - 1];
-            if (bitShift == -1) {
-                code += convert_immediate_const_to_int(matches[i]);
-            } else {
-                code += register_name_to_index(matches[i]) << bitShift;
+    if (it != configs.end()) {
+        InstructionParseConfig config = it->second;
+        code += (config.getOpcode() << 26);
+
+        std::smatch matches;
+        if (std::regex_search(command, matches, config.getRegex())) {
+            for (int i = 1; i < matches.size(); i++) {
+                int bitShift = config.getBitShifts()[i - 1];
+                if (bitShift == -1) {
+                    code += convert_immediate_const_to_int(matches[i]);
+                } else {
+                    code += register_name_to_index(matches[i]) << bitShift;
+                }
+                std::cout << std::bitset<32>(code) << std::endl;
             }
+        } else {
+            std::cerr << "Invalid instruction pattern passed as an argument." << std::endl;
         }
-    } else {
-        std::cerr << "Invalid command passed as an argument." << std::endl;
-    }
 
-    code += config.getConstantToAdd();
+        code += config.getConstantToAdd();
+    } else {
+        std::cerr << "Invalid instruction passed as an argument." << std::endl;
+    }
 
     return decimal_to_8_char_hex(code);
 }
@@ -173,38 +193,52 @@ TEST(Assembler, DecimalTo8CharHex) {
     EXPECT_EQ("00000001", decimal_to_8_char_hex(1));
     EXPECT_EQ("0000000f", decimal_to_8_char_hex(15));
     EXPECT_EQ("ffffffff", decimal_to_8_char_hex(4294967295));
+    EXPECT_EQ("0000ffff", decimal_to_8_char_hex(convert_immediate_const_to_int("0b1111111111111111")));
 }
 
 TEST(Assembler, ADDUToHexAssembly) {
     std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
-    EXPECT_EQ("01194021", convert_instruction_to_hex("ADDU $s0, $s1, $s2", configs));
-    EXPECT_EQ("00320821", convert_instruction_to_hex("ADDU $v0, $v1, $a0", configs));
+    EXPECT_EQ("02328021", convert_instruction_to_hex("ADDU $s0, $s1, $s2", configs));
+    EXPECT_EQ("00641021", convert_instruction_to_hex("ADDU $v0, $v1, $a0", configs));
 }
 
 TEST(Assembler, ADDUIToHexAssembly) {
     std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
-    EXPECT_EQ("131800ff", convert_instruction_to_hex("ADDIU $s0, $s1, 0xff", configs));
-    EXPECT_EQ("13288003", convert_instruction_to_hex("ADDIU $s1, $s2, 0b00011", configs));
-    EXPECT_EQ("1308800b", convert_instruction_to_hex("ADDIU $s1, $s0, 11", configs));
+    EXPECT_EQ("263000ff", convert_instruction_to_hex("ADDIU $s0, $s1, 0xff", configs));
+    EXPECT_EQ("26510003", convert_instruction_to_hex("ADDIU $s1, $s2, 0b00011", configs));
+    EXPECT_EQ("2611000b", convert_instruction_to_hex("ADDIU $s1, $s0, 11", configs));
+}
+
+TEST(Assembler, ANDToHexAssembly) {
+    std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
+    EXPECT_EQ("02508824", convert_instruction_to_hex("AND $s1, $s2, $s0", configs));
+    EXPECT_EQ("02328024", convert_instruction_to_hex("AND $s0, $s1, $s2", configs));
+}
+
+TEST(Assembler, ANDIToHexAssembly) {
+    std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
+    EXPECT_EQ("3251ffff", convert_instruction_to_hex("ANDI $s1, $s2, 0b1111111111111111", configs));
+    EXPECT_EQ("3230000a", convert_instruction_to_hex("ANDI $s0, $s1, 10", configs));
+    EXPECT_EQ("32320010", convert_instruction_to_hex("ANDI $s2, $s1, 0x10", configs));
 }
 
 TEST(Assembler, JRToHexAssembly) {
     std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
-    EXPECT_EQ("01f00008", convert_instruction_to_hex("JR $ra", configs));
-    EXPECT_EQ("01000008", convert_instruction_to_hex("JR $s0", configs));
-    EXPECT_EQ("01100008", convert_instruction_to_hex("JR $s1", configs));
+    EXPECT_EQ("03e00008", convert_instruction_to_hex("JR $ra", configs));
+    EXPECT_EQ("02000008", convert_instruction_to_hex("JR $s0", configs));
+    EXPECT_EQ("02200008", convert_instruction_to_hex("JR $s1", configs));
 }
 
 TEST(Assembler, LWToHexAssembly) {
     std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
-    EXPECT_EQ("4718000c", convert_instruction_to_hex("LW $s0, 12($s1)", configs));
-    EXPECT_EQ("471900fc", convert_instruction_to_hex("LW $s2, 0xFC($s1)", configs));
-    EXPECT_EQ("47190064", convert_instruction_to_hex("LW $s2, 0b1100100($s1)", configs));
+    EXPECT_EQ("8e30000c", convert_instruction_to_hex("LW $s0, 12($s1)", configs));
+    EXPECT_EQ("8e3200fc", convert_instruction_to_hex("LW $s2, 0xFC($s1)", configs));
+    EXPECT_EQ("8e320064", convert_instruction_to_hex("LW $s2, 0b1100100($s1)", configs));
 }
 
 TEST(Assembler, SWToHexAssembly) {
     std::map<std::string, InstructionParseConfig> configs = initializeConfigMap();
-    EXPECT_EQ("5718000e", convert_instruction_to_hex("SW $s0, 14($s1)", configs));
-    EXPECT_EQ("571900fd", convert_instruction_to_hex("SW $s2, 0xFD($s1)", configs));
-    EXPECT_EQ("57190064", convert_instruction_to_hex("SW $s2, 0b1100100($s1)", configs));
+    EXPECT_EQ("ae30000e", convert_instruction_to_hex("SW $s0, 14($s1)", configs));
+    EXPECT_EQ("ae3200fd", convert_instruction_to_hex("SW $s2, 0xFD($s1)", configs));
+    EXPECT_EQ("ae320064", convert_instruction_to_hex("SW $s2, 0b1100100($s1)", configs));
 }
