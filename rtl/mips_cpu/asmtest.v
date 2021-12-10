0fdf1d8af1d6e5141fa4bb2887d0e116c8795847
Václav Pavlíček <vendasky@email.cz>
2021-12-10 12:11:57 +0000

    merge fix

--- rtl/mips_cpu/asmtest.v
+++ rtl/mips_cpu/asmtest.v
@@ -1,20 +1,3 @@
-<<<<<<< HEAD:rtl/mips_cpu/asmtest.v
-module asmtest();
-
-logic[31:0] a;
-logic[31:0] b;
-logic[63:0] mult;
-
-initial begin
-    assign a = 32'hf000;
-    assign b = 32'h1e000;
-    assign mult = a*b;
-    #1
-    $display("hi=%h, lo=%h", mult[63:32], mult[31:0]);
-    $finish;
-end
-
-=======
 module asmtest();
 
 logic[31:0] a;
@@ -35,5 +18,4 @@ initial begin
     $finish;
 end
 
->>>>>>> bus-integration_ALU:src/asmtest.v
 endmodule
