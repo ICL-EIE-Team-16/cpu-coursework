0bdc7cdd171c2ab96dd110fbda59a6b72370f7db
Václav Pavlíček <vendasky@email.cz>
2021-12-11 09:44:44 +0000

    fix: convert all remaining test files

--- test/rename.sh
+++ test/rename.sh
@@ -8,21 +8,26 @@ ASSEMBLY_REGEX="./test-inputs-legacy/0-assembly/([1-9]+)-([a-z]+)"
 
 # Loop over every file matching the testcase pattern
 for i in ${TESTCASES} ; do
-    echo ${i}
     if [[ ${i} =~ $ASSEMBLY_REGEX ]]; then
-        echo "new dir - ./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}"
-        mkdir "./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}"
-        ASM_PATH="./test-inputs-legacy/0-assembly/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}*/*.asm.txt"
-        REF_V0_PATH="./test-inputs-legacy/4-reference/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}*/*-v0.ref"
-        for i in ${ASM_PATH} ; do
-          if [ -f "$i" ]; then
-            cat ${i} > ./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}.asm.txt
-          fi
-        done
+        if [ ! -d "test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}" ]
+          then
+            mkdir "test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}"
+          end
+        fi
 
-        for i in ${REF_V0_PATH} ; do
-          if [ -f "$i" ]; then
-            cat ${i} > ./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}-v0.ref
+        # ASM_PATH="./test-inputs-legacy/0-assembly/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}*"
+        REF_V0_PATH="./test-inputs-legacy/4-reference/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}*"
+
+        # for x in ${ASM_PATH} ; do
+          # if [ -f "$x" ]; then
+            # cat ${x} > ./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}.asm.txt
+          # fi
+        # done
+
+        for x in ${REF_V0_PATH} ; do
+          echo "here: ${x}"
+          if [ -f "$x" ]; then
+            cat ${x} > ./test-cases/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-${BASH_REMATCH[1]}-v0.ref
           fi
         done
     else
@@ -30,5 +35,5 @@ for i in ${TESTCASES} ; do
     fi
 done
 
-./test-inputs-legacy/0-assembly/70-sltiu-false.asm.txt
-./test-inputs-legacy/0-assembly/80-divu_lo.asm.txt
+# ./test-inputs-legacy/0-assembly/70-sltiu-false.asm.txt
+# ./test-inputs-legacy/0-assembly/80-divu_lo.asm.txt
